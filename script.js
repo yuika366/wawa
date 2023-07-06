3903f282d3fb15bab64efa239a5c3ce2132268b5
yuika366 <138746466+yuika366@users.noreply.github.com>
2023-07-06 17:40:40 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const message = 'Hello world' // Try edit me
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+// Log to console
+console.log("Hello World");
+var list=["banana","melon","persimmon","peach"];
+console.log(list);
+for (var i=0;i<　list.length;  i++){console.log(list[i]);}
+
+function Sum(){var sum=0;
+for (var i=1;i<=100;  i++)
+{sum +=i;}
+console.log("合計"+sum);}
+Sum()
